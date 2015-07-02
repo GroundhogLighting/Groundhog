016666b9d888b68b1c4a9047d0601acf5fd80740
German Molina <gmolina@igd.cl>
2015-07-02 18:52:01 -0300

    Ruined some doc... have to make it better again.

--- IGD_Groundhog/src/Rad.rb
+++ IGD_Groundhog/src/Rad.rb
@@ -5,15 +5,15 @@ module IGD
 
 			# Calculates the UDI using the simplest DC matrix
 			# @author German Molina
-			# @param bins [Integer] The sky subdivition		
+			# @param bins [Integer] The sky subdivition
 			# @return success [Boolean]
 			def self.calc_UDI(bins)
-				return false if not OS.ask_about_Radiance				
+				return false if not OS.ask_about_Radiance
 				return false if not self.calc_DC_annual_illuminance(bins)
 				path=OS.tmp_groundhog_path
-				FileUtils.cd(path) do						
+				FileUtils.cd(path) do
 					wps=Dir["Workplanes/*"]
-				
+
 					wps.each do |workplane| #calculate UDI for each workplane
 						info=workplane.split("/")
 						name=info[1].split(".")[0]
@@ -32,10 +32,10 @@ module IGD
 
 			# Calculates the annual illuminance using the simplest DC matrix
 			# @author German Molina
-			# @param bins [Integer] The sky subdivition		
+			# @param bins [Integer] The sky subdivition
 			# @return success [Boolean]
-			def self.calc_DC_annual_illuminance(bins)	
-				return false if not OS.ask_about_Radiance						
+			def self.calc_DC_annual_illuminance(bins)
+				return false if not OS.ask_about_Radiance
 				return false if not self.calc_DC(bins)
 				path=OS.tmp_groundhog_path
 				FileUtils.cd(path) do
@@ -48,16 +48,16 @@ module IGD
 					return false if not path
 
 					OS.mkdir("Results")
-								
+
 					extension=path.split(".").pop
 					UI.messagebox("Please choose a EPW or WEA file.") if extension != "wea" and extension != "epw"
 					return false if extension != "wea" and extension != "epw"
 					weaname=path.tr("\\","/").split("/").pop.tr("#{extension}","")
-				
+
 					#get the file... transform if needed
 					script << "epw2wea #{path} #{weaname}wea" if extension=="epw"
 					FileUtils.cp(path,"#{weaname}wea") if extension=="wea"
-				
+
 					wps.each do |workplane|
 						info=workplane.split("/")
 						name=info[1].split(".")[0]
@@ -66,25 +66,25 @@ module IGD
 						#WIN
 						script << "gendaymtx -m #{bins} #{weaname}wea | dctimestep DC/#{name}.dmx | rmtxop -fa - | rcollate -ho -oc 1 | rcalc -e \"$1=179*(0.265*$1+0.67*$2+0.065*$3)\" > Results/#{name}_DC.txt" if OS.getsystem=="WIN"
 					end
-				
+
 					return false if not OS.execute_script(script)
 					return true
 				end
 			end
-			
-			
+
+
 			# Calculates the simplest DC matrix
 			# @author German Molina
-			# @param bins [Integer] The sky subdivition		
+			# @param bins [Integer] The sky subdivition
 			# @return success [Boolean]
 			def self.calc_DC(bins)
-				return false if not OS.ask_about_Radiance			
-				
+				return false if not OS.ask_about_Radiance
+
 				path=OS.tmp_groundhog_path
 				return false if not Exporter.export(path)
-				
+
 				FileUtils.cd(path) do
-					if not File.directory?("Workplanes") 
+					if not File.directory?("Workplanes")
 						UI.messagebox("There are no workplanes to calculate")
 						return false
 					end
@@ -100,32 +100,32 @@ module IGD
 					winstring=Dir["Windows/*"].collect{|x| x.tr("\\","/").split("/")[-1]}.join(" ./Windows/")
 					winstring="./Windows/#{winstring}" if winstring.length > 0
 					winstring="" if winstring.length==0
-								
-					#build the script	
+
+					#build the script
 					script=[]
-					
-					wps=Dir["Workplanes/*"]		
+
+					wps=Dir["Workplanes/*"]
 					wps.each do |workplane|
 						info=workplane.split("/")
 						name=info[1].split(".")[0]
 						script << "rfluxmtx -n 1 -I+ #{Config.rcontrib_options} < #{workplane} - Skies/sky.rad Materials/materials.mat scene.rad #{winstring} > DC/#{name}.dmx"
 					end
-				
+
 					return OS.execute_script(script)
 				end
 			end
-			
-			
+
+
 			# Calculates the illuminance in the workplanes in the scene with the sun in the current position
 			# @author German Molina
-			# @param void			
+			# @param void
 			def self.actual_illuminance
-				return false if not OS.ask_about_Radiance			
+				return false if not OS.ask_about_Radiance
 				path=OS.tmp_groundhog_path
 				return false if not Exporter.export(path)
-				
+
 				FileUtils.cd(path) do
-					if not File.directory?("Workplanes") 
+					if not File.directory?("Workplanes")
 						UI.messagebox("There are no workplanes to calculate")
 						return false
 					end
@@ -137,8 +137,8 @@ module IGD
 					#	f.write("!gensky -ang 45 0 -c -B 55.86592\n\n")
 					#	f.write("skyfunc glow skyglow\n0\n0\n4 1 1 1 0\n\nskyglow source skyball\n0\n0\n 4 0 0 1 360")
 					#}
-								
-					#build the script	
+
+					#build the script
 					script=[]
 
 					#oconv
@@ -146,9 +146,9 @@ module IGD
 					winstring="./Windows/#{winstring}" if winstring.length > 0
 					winstring="" if winstring.length==0
 					script << "oconv ./Materials/materials.mat ./scene.rad #{winstring} > octree.oct"
-					
+
 					wps=Dir["Workplanes/*"]
-					results=[]				
+					results=[]
 					wps.each do |workplane|
 						info=workplane.split("/")
 						name=info[1].split(".")[0]
@@ -158,27 +158,27 @@ module IGD
 						#for Windows
 						script << "rtrace -h -I+ -af ambient.amb -oov #{Config.rtrace_options} octree.oct < #{workplane} | rcalc -e \"$1=$1; $2=$2; $3=$3; $4=179*(0.265*$4+0.67*$5+0.065*$6)\" > Results/#{name}.txt" if OS.getsystem=="WIN"
 					end
-				
+
 					success=OS.execute_script(script)
 					return if not success
-				
+
 					results.each do |res|
 						Results.import_results("Results/#{res}.txt")
 					end
-				
-					OS.clear_actual_path				
+
+					OS.clear_actual_path
 				end
 			end
 
 			# Calculates the daylight factor for the workplanes in the scene
 			# @author German Molina
-			# @param void			
+			# @param void
 			def self.daylight_factor
-				return false if not OS.ask_about_Radiance			
+				return false if not OS.ask_about_Radiance
 				path=OS.tmp_groundhog_path
 				return false if not Exporter.export(path)
 				FileUtils.cd(path) do
-					if not File.directory?("Workplanes") 
+					if not File.directory?("Workplanes")
 						UI.messagebox("There are no workplanes to calculate")
 						return false
 					end
@@ -190,8 +190,8 @@ module IGD
 						f.write("!gensky -ang 45 0 -c -B 55.86592\n\n")
 						f.write("skyfunc glow skyglow\n0\n0\n4 1 1 1 0\n\nskyglow source skyball\n0\n0\n 4 0 0 1 360")
 					}
-								
-					#build the script	
+
+					#build the script
 					script=[]
 
 					#oconv
@@ -199,9 +199,9 @@ module IGD
 					winstring="./Windows/#{winstring}" if winstring.length > 0
 					winstring="" if winstring.length==0
 					script << "oconv ./Materials/materials.mat ./scene.rad #{winstring} > octree.oct"
-					
+
 					wps=Dir["Workplanes/*"]
-					results=[]				
+					results=[]
 					wps.each do |workplane|
 						info=workplane.split("/")
 						name=info[1].split(".")[0]
@@ -211,48 +211,48 @@ module IGD
 						#for Windows
 						script << "rtrace -h -I+ -af ambient.amb -oov #{Config.rtrace_options} octree.oct < #{workplane} | rcalc -e \"$1=$1; $2=$2; $3=$3; $4=179*(0.265*$4+0.67*$5+0.065*$6)/100\" > Results/#{name}.txt" if OS.getsystem=="WIN"
 					end
-				
+
 					success=OS.execute_script(script)
 					return if not success
-				
+
 					results.each do |res|
 						Results.import_results("Results/#{res}.txt")
 					end
-				
-					OS.clear_actual_path				
+
+					OS.clear_actual_path
 				end
 			end
 
 			# Calls RVU for previewing the actual scene from the current view and sky.
 			# @author German Molina
-			# @param void	
+			# @param void
 			# @return success [Boolean]
 			def self.rvu
 				return false if not OS.ask_about_Radiance
-	
+
 				path=OS.tmp_groundhog_path
 				Exporter.export(path)
-	
+
 				success=false
-				FileUtils.cd(path) do	
+				FileUtils.cd(path) do
 					script=[]
-								
+
 					#oconv
 					winstring=Dir["Windows/*"].collect{|x| x.tr("\\","/").split("/")[-1]}.join(" ./Windows/")
 					winstring="./Windows/#{winstring}" if winstring.length > 0
 					winstring="" if winstring.length==0
 					script << "oconv ./Materials/materials.mat ./scene.rad #{winstring} > octree.oct"
-				
+
 					script << "rvu #{Config.rvu_options} -vf Views/view.vf octree.oct"
 
 					success = OS.execute_script(script)
 					OS.clear_actual_path
-				end	
+				end
 				return success
 			end
-		
-		
-	
+
+
+
 		end #end class
 	end #end module
 end
