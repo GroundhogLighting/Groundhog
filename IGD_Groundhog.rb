2e38e9735b935e4aa3e4bd2fe2fad98b4a1437cc
germolinal <germolinal@gmail.com>
2016-05-05 20:32:28 -0300

    automatic commit, built doc, wrapped RBZ, and pushed

--- IGD_Groundhog.rb
+++ IGD_Groundhog.rb
@@ -8,7 +8,7 @@ module IGD
 		Groundhog = SketchupExtension.new "Groundhog", "IGD_Groundhog/src/Groundhog_main"
 		Groundhog.copyright='Germán Molina, Sergio Vera, Waldo Bustamante'
 		Groundhog.creator='Germán Molina (gmolina@igd.cl)'
-		Groundhog.description = "OpenSource SketchUp extension for creating, exporting and analyzing Radiance Models."
+		Groundhog.description = "Open Source SketchUp extension for creating, exporting and analyzing Radiance Models."
 		Groundhog.name = 'Groundhog'
 		Groundhog.version = '0.8.9'
 		Sketchup.register_extension Groundhog, true
